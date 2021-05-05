e1a45c54c339b24f06cacd6fb2257752ac88cc21
rishabhraj123 <rishabhraj123@gmail.com>
2021-05-06 04:36:10 +0530

    Add change to roll 2 dice

--- dice_roller.py
+++ dice_roller.py
@@ -1,5 +1,16 @@
+import random
+
 def main():
-  print('You rolled a die')
+
+  dice_rolls = 2
+  print(f'You rolled {dice_rolls} dice(s)')
+  dice_sum = 0
+  for i in range(dice_rolls):
+  	roll = random.randint(1,6)
+  	print(f'You rolled a {roll}')
+  	dice_sum += roll
+
+  print(f'You rolled a total of {dice_sum}')
 
 if __name__== "__main__":
   main()

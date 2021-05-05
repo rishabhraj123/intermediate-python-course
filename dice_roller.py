2afd05102bc6d9f6e69cda3ebcb539a000db0dc9
rishabhraj123 <rishabhraj123@gmail.com>
2021-05-06 04:42:02 +0530

    Add to take user input for dice and its sides

--- dice_roller.py
+++ dice_roller.py
@@ -1,16 +1,24 @@
 import random
 
 def main():
+	dice_rolls = int(input("How many Dice would you like to roll? > "))
+	dice_size = int(input("How many sides does the dice have? > "))
+	print(f'You rolled {dice_rolls} dice(s)')
+	dice_sum = 0
 
-  dice_rolls = 2
-  print(f'You rolled {dice_rolls} dice(s)')
-  dice_sum = 0
-  for i in range(dice_rolls):
-  	roll = random.randint(1,6)
-  	print(f'You rolled a {roll}')
-  	dice_sum += roll
+	for i in range(dice_rolls):
+		roll = random.randint(1,dice_size)
 
-  print(f'You rolled a total of {dice_sum}')
+		if roll == 1:
+			print(f'You rolled a {roll}! Critical Fail')
+		elif roll == dice_size:
+			print(f'You rolled a {roll}! Critical Success!')
+		else:
+			print(f'You rolled a {roll}')
+
+		dice_sum += roll
+
+	print(f'You rolled a total of {dice_sum}')
 
 if __name__== "__main__":
   main()
